7b189889d9b70d27ea9a1a2ebf47185d322c25b0
Niarit <zsofi.deak.nia@gmail.com>
2020-09-22 16:49:20 +0200

    Add enemies to the rooms and make them intractable

--- actions.py
+++ actions.py
@@ -12,12 +12,33 @@ class Action:
         raise NotImplementedError()
 
 
-class MovementAction(Action):
+class EscapeAction(Action):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise SystemExit()
+
+
+class ActionWithDirection(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
+
         self.dx = dx
         self.dy = dy
 
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise NotImplementedError()
+
+
+class MeleeAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+        target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
+        if not target:
+            return  # No entity to attack.
+        print(f"you poke the {target.name} with a stick, much to its annoyance!")
+
+
+class MovementAction(ActionWithDirection):
     def perform(self, engine: Engine, entity: Entity) -> None:
         dest_x = entity.x + self.dx
         dest_y = entity.y + self.dy
@@ -28,9 +49,18 @@ class MovementAction(Action):
         if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return  # Destination is blocked by tile.
 
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return  # Destination is blocked by an entity.
+
         entity.move(self.dx, self.dy)
 
 
-class EscapeAction(Action):
+class BumpAction(ActionWithDirection):
     def perform(self, engine: Engine, entity: Entity) -> None:
-        raise SystemExit()
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return MeleeAction(self.dx, self.dy).perform(engine, entity)
+        else:
+            return MovementAction(self.dx, self.dy).perform(engine, entity)
--- engine.py
+++ engine.py
@@ -1,23 +1,27 @@
-from typing import Set, Iterable, Any
+from typing import Iterable, Any
 
 from tcod.context import Context
 from tcod.console import Console
 from tcod.map import compute_fov
 
 from entity import Entity
-from input_handler import EventHandler
 from game_map import GameMap
+from input_handler import EventHandler
 
 
 class Engine:
-    def __init__(self, entities: Set[Entity], event_handler: EventHandler, game_map: GameMap, player: Entity):
-        self.entities = entities
+    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
         self.event_handler = event_handler
-        self.player = player
         self.game_map = game_map
+        self.player = player
         self.update_fov()
 
-    def handle_event(self, events: Iterable[Any]) -> None:
+    def handle_enemy_turns(self) -> None:
+        pass
+        # for entity in self.game_map.entities - {self.player}:
+        #     print(f'The {entity.name} wonders when it will get to take a real turn.')
+
+    def handle_events(self, events: Iterable[Any]) -> None:
         for event in events:
             action = self.event_handler.dispatch(event)
 
@@ -25,24 +29,21 @@ class Engine:
                 continue
 
             action.perform(self, self.player)
-
-            self.update_fov()
+            self.handle_enemy_turns()
+            self.update_fov()  # Update the FOV before the players next action.
 
     def update_fov(self) -> None:
-        """Recompute the visibility of area based on the players POV"""
+        """Recompute the visible area based on the players point of view."""
         self.game_map.visible[:] = compute_fov(
             self.game_map.tiles["transparent"],
             (self.player.x, self.player.y),
-            radius=4,
+            radius=8,
         )
+        # If a tile is "visible" it should be added to "explored".
         self.game_map.explored |= self.game_map.visible
 
     def render(self, console: Console, context: Context) -> None:
         self.game_map.render(console)
-
-        for entity in self.entities:
-            if self.game_map.visible[entity.x, entity.y]:
-                console.print(entity.x, entity.y, entity.char, fg=entity.color)
 
         context.present(console)
 
